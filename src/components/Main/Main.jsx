d25f382cf0a135c114a2d95064f57a65e3fb23e6
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2025-03-30 14:02:21 +0600

    added main container bottom and css style for main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -32,6 +32,16 @@ const Main = () => {
                         <img src={assets.code_icon} alt="" />
                     </div>
                 </div>
+
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a prompt here' />
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                    <p className="bottom-info">AI Learning Agent may not give accurate answered! So double-check its responses.</p>
+                </div>
             </div>
         </div>
     );
