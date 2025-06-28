c7be981a06e8b8fbf793e9c017075e1c7ca1211c
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2025-06-29 01:36:56 +0600

    refactor HeroSection layout by removing unnecessary max-width constraint for improved responsiveness

--- src/components/LandingPage/sections/HeroSection.jsx
+++ src/components/LandingPage/sections/HeroSection.jsx
@@ -127,7 +127,7 @@ const HeroSection = () => {
             }}
             className="w-full h-full flex items-center justify-center"
           >
-            <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
               <div className="flex flex-col lg:flex-row items-center justify-between gap-12 lg:gap-16">
                 
                 {/* Left Content */}
